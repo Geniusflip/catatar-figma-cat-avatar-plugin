fd8cf68ae46dbb62f9fb38299de3e576a2c9c380
Ryan Herford <ryan.herford@servian.com>
2021-04-21 21:21:07 +1000

    first commit

---
+++ code.js
@@ -0,0 +1,24 @@
+const applyToNodes = Array.from(figma.currentPage.selection);
+figma.showUI(__html__, { visible: false });
+if (!applyToNodes.length) {
+    const element = figma.createEllipse();
+    applyToNodes.push(element);
+}
+figma.ui.postMessage({ type: 'networkRequest', num: applyToNodes.length });
+figma.ui.onmessage = (msg) => {
+    msg.images.forEach((img, i) => {
+        catify(applyToNodes[i], img);
+    });
+    figma.closePlugin();
+};
+figma.viewport.scrollAndZoomIntoView(applyToNodes);
+function catify(element, image) {
+    if (element.fills) {
+        const newPaint = JSON.parse(JSON.stringify(element.fills[0]));
+        newPaint.imageHash = figma.createImage(image).hash;
+        newPaint.type = 'IMAGE';
+        newPaint.scaleMode = 'CROP';
+        delete newPaint.color;
+        element.fills = [newPaint];
+    }
+}
